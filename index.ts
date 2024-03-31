1e641b53b381268bbe428b9f332361f6ce88a87b
IamThiago-IT <thiagodossantos315@gmail.com>
2024-03-31 11:58:13 -0300

    find users

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ const prisma = new PrismaClient()
 
 async function main() {
   // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
 }
 
 main()
